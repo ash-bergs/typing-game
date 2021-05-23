14ce55c68a03e85319ade381c5d39d7f6ca0c2f7
Ash <ashley.bergsma42@gmail.com>
2021-05-23 15:42:53 -0700

    Adds set up code
    A couple of common components have been created:
    
    - Button
    - Container
    
    The Words file just contains an array of strings, words that will be typed in-game
    
    Finally, in App we have set up several slices of state (currently commented out) that will be used in the making of this game. In this approach all the state is living in the top layer - which is App.js.

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 # Building a Typing Game with React! 
 
-This project is a simple typing game built with React. I built this to get some practice with React, explore and learn more about string manipulation, and refresh my memory of DOM mechanics. A user is given a word from a pre-made list of words, the goal is to type the most words correctly in a time limit. 
+This project is a simple typing game built with React. I built this to get some practice with React, project file structure, string manipulation, and refresh my memory of DOM mechanics. A user is given a word from a pre-made list of words, the goal is to type the most words correctly in a time limit. 
+
+This was built using a tutorial, once built I will refactor it and make it my own. There's nothing wrong with learning from our betters! 📺🏫
 
 
 ### Note on this Repository:
@@ -24,3 +26,11 @@ To get started with React, along with [Serverless Functions](https://vercel.com/
 ```shell
 $ npx create-react-app my-app
 ```
+
+### Future Versions: 
+
+- Easy, Intermediate, Expert levels - generate different lists of words based on included letters, words with x's, p's, q's for example could be in the algorithm for "expert mode" for example, or longer length words 
+
+Preliminary research: 
+
+- (medium article)[https://medium.com/jsunderthescope/generating-a-list-of-valid-words-in-javascript-efd3955c8912]
--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React /*, { useState, useEffect }*/  from 'react';
+//import Words from "./components/Words"; 
+import Container from "./components/Container"; 
+import "./App.css"
+/* -------------------------------------------------------------------------- */
+/*                            Notes on this project                           */
+/*
+  * In this tutorial `state` lives in App, e.g. the topmost layer. This is an example of `lifting state`
+*/
+/* -------------------------------------------------------------------------- */
 
 function App() {
-  
+  //! STATE !\\
+  // words is set to the object exported from the Words.js file
+  //? I think a more appropriate name for this state would be words (plural) ? 
+  // const [word, setWord] = useState(Words); 
+  // initial state is the first word in Words object
+  // const [newWord, setNewWord] = useState(word[0])
+  // disabled toggles gameplay - starts the timer 
+  // const [disabled, setDisabled] = useState(true); 
+  // currentResults is an array that holds the words the user has encountered, wrong or right 
+  //? these states have inconsistent names I might want to revisit later (for now I'll continue with the tutorial to avoid confusion)
+  // const [currentResults, setCurrentResults] = useState([]);
+  // const [wrongResults, setWrongResults] = useState([]); 
+  // const [countCorrect, setCountCorrect] = useState(0); 
+  // const [time, setTime] = useState(30); 
+  // inputValue is the answer the user submits, what has been typed into the form component to come later 
+  // const [inputValue, setInputValue] = useState(""); 
+  // const [animation, setAnimation] = useState(null); 
+
+  // let randomWord = Math.floor(Math.random() * word.length); 
+
+
   return (
-    <main>
-      <p>Typing game! <span>⌨</span></p>
-    </main>
+    <div className="App">
+      <Container>
+
+      </Container>
+    </div>
   );
 }
 
