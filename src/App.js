9b3ec03a2c20ee0ba9e2025062564fbc379fdfb7
Ash <ashley.bergsma42@gmail.com>
2021-05-23 16:06:23 -0700

    Connected time & input state
    Props passed and destructured in Typeracer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React /*, { useState, useEffect }*/  from 'react';
-//import Words from "./components/Words"; 
+import React, { useState, useEffect }  from 'react';
+import Words from "./components/Words"; 
 import Container from "./components/Container"; 
 import Typeracer from "./components/Typeracer"; 
 import "./App.css"
@@ -14,20 +14,20 @@ function App() {
   //! STATE !\\
   // words is set to the object exported from the Words.js file
   //? I think a more appropriate name for this state would be words (plural) ? 
-  // const [word, setWord] = useState(Words); 
+  const [word, setWord] = useState(Words); 
   // initial state is the first word in Words object
-  // const [newWord, setNewWord] = useState(word[0])
+  const [newWord, setNewWord] = useState(word[0])
   // disabled toggles gameplay - starts the timer 
-  // const [disabled, setDisabled] = useState(true); 
+  const [disabled, setDisabled] = useState(true); 
   // currentResults is an array that holds the words the user has encountered, wrong or right 
   //? these states have inconsistent names I might want to revisit later (for now I'll continue with the tutorial to avoid confusion)
   // const [currentResults, setCurrentResults] = useState([]);
   // const [wrongResults, setWrongResults] = useState([]); 
   // const [countCorrect, setCountCorrect] = useState(0); 
-  // const [time, setTime] = useState(30); 
+  const [time, setTime] = useState(30); 
   // inputValue is the answer the user submits, what has been typed into the form component to come later 
-  // const [inputValue, setInputValue] = useState(""); 
-  // const [animation, setAnimation] = useState(null); 
+  const [inputValue, setInputValue] = useState(""); 
+  const [animation, setAnimation] = useState(null); 
 
   // let randomWord = Math.floor(Math.random() * word.length); 
 
@@ -35,7 +35,14 @@ function App() {
   return (
     <div className="App">
       <Container>
-        <Typeracer />
+        <Typeracer 
+        newWord={newWord}
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+        disabled={disabled}
+        time={time}
+        animation={animation}
+        />
       </Container>
     </div>
   );
--- src/components/Typeracer.js
+++ src/components/Typeracer.js
@@ -3,18 +3,38 @@ import Button from './Button';
 
 /* ----------------------------------- 📓 ----------------------------------- */
 //* the state for values in the component come from App! 
+//! the ANIMATION property of the time display is a slice of state passed from App
+// It is null on initialization, but when the timer reaches 10 seconds or less it begins 
+// This is accomplished by updating the animation state dynamically. 
+// In the STYLE property of the `time` div we use a ternary to assign the animation to the one defined in App.css, otherwise it's an empty string (no animation) 
 
-const Typeracer = () => {
+const Typeracer = (props) => {
+    const { newWord, 
+        inputValue, 
+        setInputValue, 
+        disabled, 
+        time, 
+        animation } = props; 
+
     return (
         <div className="typeRacer">
             <div className="wordOutput">
-                <p>New Word</p>
+                <p>{newWord}</p>
             </div>
-            <div className="time">
-                <p>Time</p>
+            <div 
+            style={{ animation: animation !== null ? animation : "" }}
+            className="time"
+            >
+                <p>{time}</p>
             </div>
             <div className="wordValues">
-                <input type="text" />
+                <input 
+                type="text"
+                disabled={disabled && disabled}
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                placeholder={disabled ? "" : "Start typing..."} 
+                />
                 <Button />
             </div>
         </div>
